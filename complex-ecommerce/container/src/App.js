18f811b7e29b29031abe75ad80305642724b0c1e
Helton Carlos de Souza <heltoncarlossouza@gmail.com>
2022-06-16 09:44:33 -0300

    feat: add complex-ecommerce container and marketing apps

---
+++ complex-ecommerce/container/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+
+export default () => (
+  <div>
+    <h1>Hi, there</h1>
+    <hr />
+    <MarketingApp />
+  </div>
+)
